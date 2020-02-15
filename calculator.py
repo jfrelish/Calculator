c089d7111a8f98fb82380076bf81c3a2529d5722
jeffrey <jpf37@njit.edu>
2020-02-15 09:30:18 -0500

    task added.py

---
+++ calculator.py
@@ -0,0 +1,15 @@
+number_1 = float(input('first number'))
+number_2 = float(input('second number'))
+
+print('{} + {} = '.format(number_1, number_2))
+print(number_1 + number_2)
+
+print('{} * {} = ' .format(number_1, number_2))
+print(number_1 * number_2)
+
+print('{} - {} = ' .format(number_1, number_2))
+print(number_1 - numbber_2)
+
+print('{} / {} = ' .format(number_1, number_2))
+print(number_1 / number_2)
+
