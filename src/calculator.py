b33ddfd366a93755400ff12a429792ad3ac12c3d
jeffrey <jpf37@njit.edu>
2020-02-25 09:02:35 -0500

    task edited and added more functions

--- src/calculator.py
+++ src/calculator.py
@@ -1,5 +1,29 @@
-number_1: float = float(input('first number'))
-number_2: float = float(input('second number'))
+print("Select operator")
+print("+")
+print("-")
+print("/")
+print("*")
+
+choice = input("Select operator") 
+
+number_1 = float(input('first number'))
+number_2 = float(input('second number'))
+
+if choice == '+':
+    print(number_1 + number_2)
+    
+elif choice == '-':
+   print(number_1 - numbber_2)
+   
+elif choice == '*':
+    print(number_1 * number_2)
+    
+elif choice == '/':
+    print(number_1 / number_2)
+
+else:
+    print("try again"
+
 
 print('{} + {} = '.format(number_1, number_2))
 print(number_1 + number_2)
@@ -8,8 +32,10 @@ print('{} * {} = ' .format(number_1, number_2))
 print(number_1 * number_2)
 
 print('{} - {} = ' .format(number_1, number_2))
-print(number_1 - number_2)
+print(number_1 - numbber_2)
 
 print('{} / {} = ' .format(number_1, number_2))
 print(number_1 / number_2)
+
+
 
