478619ba5d330f9b7f7352d8f6082ad3ca0dd107
jeffrey <jpf37@njit.edu>
2020-03-01 10:53:41 -0500

    Task: edited calculator.py

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -1,9 +1,14 @@
 import unittest
-
+From Calculator import Calculator
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_instantiate_calculator(self):
+        Calculator = Calculator()
+        self.assertIsInstance(Calculator, Calculator)
+
+    def test_results_property_calculator(self):
+        Calculator = Calculator()
+        self.assertEqual(Calculator.add(4, 4), 8)
 
 
 if __name__ == '__main__':
--- src/calculator.py
+++ src/calculator.py
@@ -1,30 +1,12 @@
-print("Select operator")
-print("+")
-print("-")
-print("/")
-print("*")
+def addition(a, b):
+    return a + b
+class calculator:
+    result = 0;
 
-choice = input("Select operator") 
+    def __init__(self):
+        x = 2 + 2
+        self.result = x
+        pass
 
-number_1 = float(input('first number'))
-number_2 = float(input('second number'))
-
-if choice == '+':
-    print('{} + {} = '.format(number_1, number_2))
-    print(number_1 + number_2)
-
-elif choice == '*':
-    print('{} * {} = ' .format(number_1, number_2))
-    print(number_1 * number_2)
-
-elif choice == '-':
-    print('{} - {} = ' .format(number_1, number_2))
-    print(number_1 - numbber_2)
-
-elif choice == '/':
-
-    print('{} / {} = ' .format(number_1, number_2))
-    print(number_1 / number_2)
-
-else:
-    print("try again")
+    def addition(self, a, b):
+        self.result = a + b
